839e0092511fc335a6f8df22893847f4124fbd9b
jiancong.zjc <jiancong.zjc@alibaba-inc.com>
2022-04-27 10:43:53 +0800

    AliyunQueenDemo

--- LiteAVSDK_TRTC_iOS_9.8.0.11519/TRTC-API-Example-OC/Advanced/ThirdBeauty/ThirdBeautyEntranceViewController.m
+++ LiteAVSDK_TRTC_iOS_9.8.0.11519/TRTC-API-Example-OC/Advanced/ThirdBeauty/ThirdBeautyEntranceViewController.m
@@ -8,9 +8,12 @@
 #import "ThirdBeautyBytedViewController.h"
 #import "ThirdBeautyEntranceViewController.h"
 #import "ThirdBeautyViewController.h"
+#import "ThirdBeautyAliQueenViewController.h"
 @interface ThirdBeautyEntranceViewController ()
 @property(nonatomic, strong) UIButton *beautyButton;
 @property(nonatomic, strong) UIButton *bytedButton;
+@property(nonatomic, strong) UIButton *aliQueenButton;
+
 @end
 
 @implementation ThirdBeautyEntranceViewController
@@ -18,8 +21,25 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.title = Localize(@"TRTC-API-Example.Home.ThirdBeauty");
+  [self.view addSubview:self.aliQueenButton];
   [self.view addSubview:self.beautyButton];
   [self.view addSubview:self.bytedButton];
+}
+
+- (UIButton *)aliQueenButton {
+    if (!_aliQueenButton) {
+        _aliQueenButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _aliQueenButton.frame = CGRectMake((self.view.frame.size.width - 300) / 2,
+                                           self.view.frame.size.height * 0.5 - 75 - 75, 300, 50);
+        [_aliQueenButton setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+        _aliQueenButton.layer.cornerRadius = 5;
+        _aliQueenButton.backgroundColor = [UIColor greenColor];
+        [_aliQueenButton setTitle:@"AliQueenButton" forState:UIControlStateNormal];
+        [_aliQueenButton addTarget:self
+                            action:@selector(clickAliQueenButton)
+                  forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _aliQueenButton;
 }
 
 - (UIButton *)beautyButton {
@@ -65,6 +85,13 @@
       [[ThirdBeautyBytedViewController alloc] initWithNibName:@"ThirdBeautyBytedViewController"
                                                        bundle:nil];
   [self.navigationController pushViewController:controller animated:true];
+}
+
+- (void)clickAliQueenButton {
+    UIViewController *controller =
+    [[ThirdBeautyAliQueenViewController alloc] initWithNibName:@"ThirdBeautyAliQueenViewController"
+                                                        bundle:nil];
+    [self.navigationController pushViewController:controller animated:true];
 }
 
 @end
