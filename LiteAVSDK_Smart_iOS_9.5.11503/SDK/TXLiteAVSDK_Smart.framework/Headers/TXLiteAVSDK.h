c3ce66a8d5344e1b2a95167e8e92585bb37675ec
jiancong.zjc <jiancong.zjc@alibaba-inc.com>
2022-06-01 17:23:31 +0800

    LiteAVSDK_Smart_iOS_9.5.11503

---
+++ LiteAVSDK_Smart_iOS_9.5.11503/SDK/TXLiteAVSDK_Smart.framework/Headers/TXLiteAVSDK.h
@@ -0,0 +1,42 @@
+//
+//  TXLiteAVSDK.h
+//  TXLiteAVSDK
+//
+//  Created by alderzhang on 2017/6/9.
+//  Copyright © 2017年 Tencent. All rights reserved.
+//
+
+
+
+#import <TXLiteAVSDK_Smart/TXAudioCustomProcessDelegate.h>
+#import <TXLiteAVSDK_Smart/TXAudioEffectManager.h>
+#import <TXLiteAVSDK_Smart/TXAudioRawDataDelegate.h>
+#import <TXLiteAVSDK_Smart/TXBeautyManager.h>
+#import <TXLiteAVSDK_Smart/TXBitrateItem.h>
+#import <TXLiteAVSDK_Smart/TXDeviceManager.h>
+#import <TXLiteAVSDK_Smart/TXImageSprite.h>
+#import <TXLiteAVSDK_Smart/TXLiteAVCode.h>
+#import <TXLiteAVSDK_Smart/TXLiveAudioSessionDelegate.h>
+#import <TXLiteAVSDK_Smart/TXLiveBase.h>
+#import <TXLiteAVSDK_Smart/TXLivePlayConfig.h>
+#import <TXLiteAVSDK_Smart/TXLivePlayListener.h>
+#import <TXLiteAVSDK_Smart/TXLivePlayer.h>
+#import <TXLiteAVSDK_Smart/TXLivePush.h>
+#import <TXLiteAVSDK_Smart/TXLivePushConfig.h>
+#import <TXLiteAVSDK_Smart/TXLivePushListener.h>
+#import <TXLiteAVSDK_Smart/TXLiveRecordListener.h>
+#import <TXLiteAVSDK_Smart/TXLiveRecordTypeDef.h>
+#import <TXLiteAVSDK_Smart/TXLiveSDKEventDef.h>
+#import <TXLiteAVSDK_Smart/TXLiveSDKTypeDef.h>
+#import <TXLiteAVSDK_Smart/TXPlayerAuthParams.h>
+#import <TXLiteAVSDK_Smart/TXVideoCustomProcessDelegate.h>
+#import <TXLiteAVSDK_Smart/TXVodPlayConfig.h>
+#import <TXLiteAVSDK_Smart/TXVodPlayListener.h>
+#import <TXLiteAVSDK_Smart/TXVodPlayer.h>
+#import <TXLiteAVSDK_Smart/V2TXLiveCode.h>
+#import <TXLiteAVSDK_Smart/V2TXLiveDef.h>
+#import <TXLiteAVSDK_Smart/V2TXLivePlayer.h>
+#import <TXLiteAVSDK_Smart/V2TXLivePlayerObserver.h>
+#import <TXLiteAVSDK_Smart/V2TXLivePremier.h>
+#import <TXLiteAVSDK_Smart/V2TXLivePusher.h>
+#import <TXLiteAVSDK_Smart/V2TXLivePusherObserver.h>
